43406b06e0e0cd121cb61505cf4d7c54288d6d0f
Tolulope Aina-Badejo <61244960+folusara@users.noreply.github.com>
2021-09-04 22:20:35 +0100

    seconf

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,17 +5,24 @@
       <div id="welDiv">
             <h1>Welcome to the World's Number One Blog</h1>
           <p>Stay updated with the latest on politics,sports, religion, finance and Technology. </p>
-           <el-button type="danger" round> Learn More   <span class="fa fa-hand-point-right"></span></el-button>       
-
-         
+          <router-link class="rout" to="/allblogs"> <el-button type="danger" round> View Blogs  <span class="fa fa-hand-point-right"></span>  </el-button> </router-link>        
       </div>
       <div  id="picDiv" >
         <img src="../assets/img/blogPic.png" alt="">
       </div>
   </div>
-   <Footer  />
   
+   <div id="second">
+      <h1 >Publish your passions, your way!</h1>
+      <p>Create a unique and beautiful blog. It's easy and totally free.</p>
+    <router-link class="rout" to="/create">   <el-button type="danger" round>  Create Blog  <span class="fa fa-plus"></span> </el-button>   </router-link>      
+
+   </div>
+  
+   <!-- <Footer  /> -->
+
 </main>
+
 
 
 
@@ -47,6 +54,35 @@ export default {
   padding-left: 170px ;
 }
 
+#second{
+  margin-top: 300px;
+  height: 350px;
+  width: 100%;
+  background-color: rgb(28,35,44);
+  color: white;
+  text-align: center;
+  padding-top: 100px;
+}
+
+.rout{
+color: white;
+}
+
+.rout:hover{
+  color:rgb(245,108,108) ;
+  background-color: white;
+  border-radius: 40px;
+}
+/* :link{
+    text-decoration: none;
+    font-weight: 300;
+color: white;
+} */
+
+a{
+  color: white;
+}
+
 #picDiv{
   margin-top: -50px;
 }
@@ -62,6 +98,14 @@ font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubun
 #welDiv  > p{
   font-weight: 20;
   font-size: 30px;
+}
+#second > h1{
+font-size: 50px;
+font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+#second  > p{
+  font-weight: 20;
+  font-size: 20px;
 }
 #learn{
   margin-top: 70px;
